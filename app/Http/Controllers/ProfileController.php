8448e0ce34a2986126b378ff4184fc6e1bb866f2
thera1DDD <tagir566666@gmail.com>
2024-03-29 14:59:01 +0300

    initial office commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,8 +14,6 @@ class ProfileController extends Controller
     /**
      * Display the user's profile form.
      */
-
-
     public function edit(Request $request): View
     {
         return view('profile.edit', [
